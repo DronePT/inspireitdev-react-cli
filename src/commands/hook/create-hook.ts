6c513e9d3282bb9e462388a148573ba247c81624
André Alves <andre.alves@inspireit.pt>
2021-11-05 01:41:03 +0000

    :sparkles: Domain hook: Ask for confirmation to create resources.

--- src/commands/hook/create-hook.ts
+++ src/commands/hook/create-hook.ts
@@ -2,27 +2,49 @@ import { Command } from 'commander';
 import { toCamelCase } from '../../utils/camel-case';
 import { createDirectory, createModulePath } from '../../utils/directory';
 import { createExportFile, createFile } from '../../utils/file';
+import { Tasks } from '../../utils/tasks';
 import { getFromTemplate } from '../../utils/template';
 import { toHyphen } from '../../utils/to-hyphen';
 
 export const createHookAction =
   (program: Command) =>
-  async (moduleName: string, hookName: string, options: any) => {
-    const hookPath = createDirectory([
-      createModulePath(program, moduleName),
-      'hooks',
-    ]);
+  async (
+    moduleName: string,
+    hookName: string,
+    options: { force?: boolean },
+  ) => {
+    const opts = {
+      ...program.opts<{ destination: string }>(),
+      ...options,
+      customDirectory: moduleName.includes('/'),
+    };
+
+    const tasks = Tasks.create();
+
+    const modulePath = opts.customDirectory
+      ? createDirectory(moduleName.split('/'))
+      : createModulePath(program, moduleName);
+
+    tasks.add('create-path', modulePath.data, modulePath.exec);
+
+    const hookPath = createDirectory([modulePath.data, 'hooks']);
+
+    tasks.add('create-path', hookPath.data, hookPath.exec);
 
     const hook = toHyphen(hookName);
 
-    await createFile(
-      hookPath,
+    const fileToCreate = createFile(
+      hookPath.data,
       `use-${hook}.hook.ts`,
       getFromTemplate([__dirname, 'create-hook.tpl'], {
         hook: toCamelCase(hook),
       }),
-      options?.force === true,
+      opts?.force === true,
     );
 
-    await createExportFile(hookPath); // export hook
+    tasks.add('create-file', fileToCreate.data, fileToCreate.exec);
+
+    if (await tasks.run()) {
+      await createExportFile(hookPath.data); // export hook
+    }
   };

e687d022991c98baac3368c3d2d74ad85fab0e16
André Alves <andre.alves@inspireit.pt>
2021-11-07 16:54:01 +0000

    :bug: Fix create shared component

--- src/commands/component/create-shared-component.ts
+++ src/commands/component/create-shared-component.ts
@@ -1,6 +1,7 @@
 import { toCamelCase } from '../../utils/camel-case';
 import { createDirectory } from '../../utils/directory';
 import { createExportFile, createFile } from '../../utils/file';
+import { Tasks } from '../../utils/tasks';
 import { getFromTemplate } from '../../utils/template';
 
 interface CreateSharedComponentOptions {
@@ -13,6 +14,8 @@ export const createSharedComponent = async (
   componentOrModule: string,
   options: CreateSharedComponentOptions,
 ): Promise<void> => {
+  const tasks = Tasks.create();
+
   const component = toCamelCase(componentOrModule);
 
   const componentPath = createDirectory([
@@ -21,12 +24,18 @@ export const createSharedComponent = async (
     component,
   ]);
 
-  await createFile(
+  tasks.add('create-path', componentPath.data, componentPath.exec);
+
+  const createdFile = createFile(
     componentPath.data,
     `${component}.tsx`,
     getFromTemplate([__dirname, 'create-component.tpl'], { component }),
     options?.force === true,
   );
 
-  await createExportFile(componentPath.data, 'components'); // export file
+  tasks.add('create-file', createdFile.data, createdFile.exec);
+
+  if (await tasks.run()) {
+    await createExportFile(componentPath.data, 'components'); // export file
+  }
 };
